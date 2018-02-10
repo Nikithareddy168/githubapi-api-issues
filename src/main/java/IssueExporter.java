6c746583b3a70df516760c9bccd29ca8a84684e8
Nikithareddy168 <nikitha.reddy168@gmail.com>
2018-02-10 01:05:34 -0500

    Add user and assignee to the issues

--- src/main/java/IssueExporter.java
+++ src/main/java/IssueExporter.java
@@ -17,18 +17,18 @@ public class IssueExporter {
          Issue issue1 = new Issue();
          Issue issue2 = new Issue();
          Issue issue3 = new Issue();
-         User user1 = new User();
-         User user2 = new User();
-         User assignee1 = new User();
-         User assignee2 = new User();
+         User user = new User();
+
+         User assignee = new User();
+         user.setLogin("Nikithareddy168");
+         assignee.setLogin("ms-o");
          List<Issue> issuesList = new ArrayList<>();
         // System.out.println(issuesList.size());
          issue1.setNumber(1);
          issue1.setId(7067839);
-         user1.setLogin("Nikithareddy168");
-         assignee1.setLogin("ms-o");
-         issue1.setUser(user1);
-         issue1.setAssignee(assignee1);
+        
+         issue1.setUser(user);
+         issue1.setAssignee(assignee);
          issue1.setId(7067839);
          issue1.setState("open");
          issue1.setTitle("# Issue 1");
@@ -41,6 +41,8 @@ public class IssueExporter {
          
          issue2.setNumber(2);
          issue2.setId(7067839);
+         issue2.setUser(user);
+         issue2.setAssignee(assignee);
          issue2.setState("closed");
          issue2.setTitle("# Issue 2");
          issue2.setBody("This is Issue 2");
@@ -50,6 +52,8 @@ public class IssueExporter {
          issuesList.add(issue2);
          issue3.setNumber(3);
          issue3.setId(7067839);
+         issue3.setUser(user);
+         issue3.setAssignee(assignee);
          issue3.setState("open");
          issue3.setTitle("# Issue 3");
          issue3.setBody("This is Issue 3");
