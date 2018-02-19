5b10e39b202eae9309eaa349181f8b1b8689b6f8
Nikithareddy168 <nikitha.reddy168@gmail.com>
2018-02-18 20:06:45 -0500

    Failing test for parsing Json data

--- src/main/java/IssueParser.java
+++ src/main/java/IssueParser.java
@@ -1,6 +1,12 @@
 package main.java;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IssueParser {
     
-
+public List<String> parseIssues(String str){
+    List<String> isueList = new ArrayList<String>();
+    return isueList;
+}
 }
--- src/test/java/IssueParsertest.java
+++ src/test/java/IssueParsertest.java
@@ -1,34 +1,57 @@
 package test.java;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import main.java.IssueParser;
+
 
 public class IssueParsertest {
-
+    String key = "";
     @Before
-    public void setUp() {
-        try {
-            File file = new File("sample-output.txt");
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            StringBuffer stringBuffer = new StringBuffer();
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                    stringBuffer.append(line);
-                    stringBuffer.append("\n");
+    public void setUp() throws IOException {
+        {
+            FileReader file = new FileReader("C:\\Users\\Nikitha\\Downloads\\sample-output.txt");
+            BufferedReader reader = new BufferedReader(file);
+
+            // **** key is declared here in this block of code
+            
+            String line = null;
+            try {
+                line = reader.readLine();
             }
-            fileReader.close();
-            System.out.println("Contents of file:");
-            System.out.println(stringBuffer.toString());
-    } catch (IOException e) {
-            e.printStackTrace();
+            catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            while (line != null) {
+                key += "\n"+line;
+                line = reader.readLine();
+            }
+            System.out.println(key); // so key works
+        }
     }
+    @Test
+     public void parseIssuetest(){
+         IssueParser isp = new IssueParser();
+         List<String> ls1 = new ArrayList<String>();
+         ls1 = isp.parseIssues(key);
+         assertEquals(3, ls1.size());
+     }
+    @Test
+    public void noOfIssuesTest(){
+        IssueParser isp = new IssueParser();
     }
+    
 
 }
